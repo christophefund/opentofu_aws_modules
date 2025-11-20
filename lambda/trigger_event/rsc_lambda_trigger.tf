a152b13e1ed38982a61487e45d03ccc3ef9b8c88
christophefund <boldnightowl@gmail.com>
2025-11-20 17:04:58 +0100

    Added Lambda EventBridge Trigger

---
+++ lambda/trigger_event/rsc_lambda_trigger.tf
@@ -0,0 +1,25 @@
+#------------------------------------------------------------------------------
+# Create Lambda Function Trigger
+#------------------------------------------------------------------------------
+resource "aws_cloudwatch_event_rule" "this" {
+  name          = var.rule_name != null ? var.rule_name : null
+  description   = var.rule_description
+  event_pattern = var.event_pattern
+}
+
+resource "aws_cloudwatch_event_target" "this" {
+  rule      = aws_cloudwatch_event_rule.this.name
+  target_id = "LambdaTarget"
+  arn       = var.lambda_function_arn
+}
+
+#------------------------------------------------------------------------------
+# Create Lambda Permissions to Allow the Trigger
+#------------------------------------------------------------------------------
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.this.arn
+}
---
+++ lambda/trigger_event/variables.tf
@@ -0,0 +1,38 @@
+#------------------------------------------------------------------------------
+# General
+#------------------------------------------------------------------------------
+variable "tags" {
+  description = "Tags to set for the Resources"
+  type        = map(string)
+}
+
+
+#------------------------------------------------------------------------------
+# Lambda Function Trigger
+#------------------------------------------------------------------------------
+variable "lambda_function_arn" {
+  description = "ARN of the Lambda function to trigger"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function to allow EventBridge invocation"
+  type        = string
+}
+
+variable "event_pattern" {
+  description = "EventBridge event pattern JSON string"
+  type        = string
+}
+
+variable "rule_name" {
+  description = "Name of the EventBridge rule"
+  type        = string
+  default     = null
+}
+
+variable "rule_description" {
+  description = "Description of the EventBridge rule"
+  type        = string
+  default     = ""
+}
